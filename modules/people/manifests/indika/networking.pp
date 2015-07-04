bde285a1b3e369ce06094068ec9b0c494795bfc2
Indika Piyasena <indika@cobalt.local>
2015-07-05 04:34:09 +1000

    error free boxen

--- modules/people/manifests/indika/networking.pp
+++ modules/people/manifests/indika/networking.pp
@@ -9,6 +9,14 @@ class people::indika::networking {
     group => 'wheel',
   }
 
+  file { 'ssh_home':
+    ensure   => directory,
+    path     => '/Users/indika/.ssh',
+    owner    => 'indika',
+    group    => 'staff',
+    mode     => 755, #TODO: Find out what this should be,
+  }
+
   file { '/Users/indika/.ssh/config':
     ensure => present,
     mode => 600,
@@ -16,6 +24,7 @@ class people::indika::networking {
     recurse => false,
     owner => 'indika',
     group => 'staff',
+    require => File['ssh_home']
   }
 
   file { 'ssh_config_control':
@@ -24,7 +33,7 @@ class people::indika::networking {
     owner    => 'indika',
     group    => 'staff',
     mode     => 700,
+    require => File['ssh_home']
   }
-
 
 }
