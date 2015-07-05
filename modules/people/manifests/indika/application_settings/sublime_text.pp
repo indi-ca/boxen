bcd3fbf1906aa6ff1840b3a1cbd6dc2655deeb25
Indika Piyasena <indika@cobalt.local>
2015-07-05 20:56:50 +1000

    sublime keymap and preferences

--- modules/people/manifests/indika/application_settings/sublime_text.pp
+++ modules/people/manifests/indika/application_settings/sublime_text.pp
@@ -2,4 +2,21 @@ class people::indika::application_settings::sublime_text {
   sublime_text::package { 'SideBarEnhancements':
     source => 'titoBouzout/SideBarEnhancements'
   }
+
+  file { '/Users/indika/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap':
+    ensure => present,
+    mode => 644,
+    source => 'puppet:///modules/people/sublime_user_keymap.json',
+    owner => 'indika',
+    group => 'staff',
+  }
+
+  file { '/Users/indika/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings':
+    ensure => present,
+    mode => 644,
+    source => 'puppet:///modules/people/sublime_user_preferences.json',
+    owner => 'indika',
+    group => 'staff',
+  }
+
 }
