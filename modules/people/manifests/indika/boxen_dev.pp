92d06a2474878d2f4d504e453513d7dfc18c20fe
Indika Piyasena <indika@hydrasqr.com>
2015-07-11 17:50:06 +1000

    neater construction of paths

--- modules/people/manifests/indika/boxen_dev.pp
+++ modules/people/manifests/indika/boxen_dev.pp
@@ -1,16 +1,18 @@
 class people::indika::boxen_dev {
 
+    $boxen_dev_dir = "/Users/${::boxen_user}/dev/osx-boxen"
+
     # Root directory
     file { 'dir_osx-boxen':
       ensure   => directory,
-      path     => '/Users/indika/dev/osx-boxen/',
+      path     => $boxen_dev_dir,
       owner    => 'indika',
       group    => 'staff',
       mode     => 755, #TODO: Find out what this should be,
     }
 
     # Github's Boxen
-    vcsrepo { "/Users/indika/dev/osx-boxen/their-boxen":
+    vcsrepo { "${boxen_dev_dir}/their-boxen":
       ensure   => present,
       provider => git,
       source   => "https://github.com/boxen/our-boxen",
@@ -21,7 +23,7 @@ class people::indika::boxen_dev {
     }
 
     # Felho's Boxen
-    vcsrepo { "/Users/indika/dev/osx-boxen/felho-boxen":
+    vcsrepo { "${boxen_dev_dir}/felho-boxen":
       ensure   => present,
       provider => git,
       source   => "https://github.com/felho/boxen.git",
@@ -31,10 +33,8 @@ class people::indika::boxen_dev {
       require  => File['dir_osx-boxen']
     }
 
-    # This is a module that I'm developing
-    # It's kindof empty
-    # TODO: Consider deleting this and sourcing upstream
-    vcsrepo { "/Users/indika/dev/osx-boxen/puppet-virtualenv":
+    # This is a module that I'm developing. It's kindof empty
+    vcsrepo { "${boxen_dev_dir}/puppet-virtualenv":
       ensure   => present,
       provider => git,
       source   => "https://github.com/indika/puppet-virtualenv.git",
