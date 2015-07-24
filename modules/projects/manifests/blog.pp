f11a5733bddffb0ec507d089302b374649602c90
Indika Piyasena <indika@hydrasqr.com>
2015-07-25 03:09:19 +1000

    blog

--- modules/projects/manifests/blog.pp
+++ modules/projects/manifests/blog.pp
@@ -1,18 +1,20 @@
 class projects::blog {
 
-  file { 'dir_blog':
-    ensure   => directory,
-    path     => '/Users/indika/dev/blog',
+  # What is wrong with this is that I'm installing Jekyll into a global Ruby rbenv
+
+  vcsrepo { "/Users/indika/dev/blog":
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/indika/blog.git',
+    revision => '222',
     owner    => 'indika',
     group    => 'staff',
-    mode     => 755,#
   }
-
 
   # ensure a certain ruby version is used in a dir
   ruby::local { '/Users/indika/dev/blog':
     version => '2.2.2',
-    require => File['dir_blog']
+    require => Vcsrepo['/Users/indika/dev/blog']
   }
 
   # ensure a gem is installed for a certain ruby version
@@ -20,6 +22,10 @@ class projects::blog {
   $version = "2.2.2"
   ruby_gem { "jekyll for ${version}":
     gem          => 'jekyll',
+    ruby_version => $version,
+  }
+  ruby_gem { "rdiscount for ${version}":
+    gem          => 'rdiscount',
     ruby_version => $version,
   }
 
