5d44463aef67e85df259717218d79c688c777fff
Indika Piyasena <indika@hydrasqr.com>
2015-09-24 22:35:21 +1000

    tutorial added

--- modules/projects/manifests/tutorials.pp
+++ modules/projects/manifests/tutorials.pp
@@ -9,4 +9,13 @@ class projects::tutorials {
     group    => 'staff',
   }
 
+  vcsrepo { "/Users/indika/dev/tutorials/nicta-course":
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/indika/course',
+    revision => 'master',
+    owner    => 'indika',
+    group    => 'staff',
+  }
+
 }
